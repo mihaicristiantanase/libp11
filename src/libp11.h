93bee078fd998eb653c34d2f5b4bae217ecccc10
Mihai Cristian Tanase <mihaicristian.tanase@gmail.com>
2024-10-08 17:28:14 +0300

    Add documentation to parameter

--- src/libp11.h
+++ src/libp11.h
@@ -437,7 +437,7 @@ extern void ERR_load_PKCS11_strings(void);
  * Generate a private key on the token
  *
  * @param token token returned by PKCS11_find_token()
- * @param algorithm
+ * @param algorithm key pair generation algorithm (ie, mechanism)
  * @param bits size of the modulus in bits
  * @param label label for this key
  * @param id bytes to use as the id value
